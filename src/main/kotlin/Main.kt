49535197886408a7db73cc42555b723f998aa8ac
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-26 22:39:18 +0100

    declaracion clases

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+
+class Pila<T>(list: List<T>){
+    val list = list.toMutableList()
+
+    fun tope():T? = if (list.size>0) list.first() else null
+
+    fun push(param:T) = list.add(param)
+
+    fun pop(param:T):Boolean = list.remove(list.first())
+
+    fun empty():Boolean = list.isEmpty()
+
+    override fun toString(): String {
+        return list.toString()
+    }
+}
+
+
+
+
+
+
+fun main() {
+
+    val a = Pila<Int>(2,3,4)
 }
